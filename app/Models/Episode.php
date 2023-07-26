48d6928df798bbfc6fdf37b53961b4c73a8f7156
Mr.Robot <macbookpro@MacBook-Pro-de-MacBook.local>
2023-07-26 15:54:59 +0100

    first update

---
+++ app/Models/Episode.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Course;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Episode extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'description', 'video_url', 'course_id', 'duration'];
+
+    public function course()
+    {
+        return $this->belongsTo(Course::class);
+    }
+
+        public function users()
+    {
+        return $this->belongsToMany(User::class, 'completions', 'episode_id', 'user_id');
+    }
+}
---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'coder',
+            'email' => 'coder@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make(12345678), // password
+            'two_factor_secret' => null,
+            'two_factor_recovery_codes' => null,
+            'remember_token' => Str::random(10),
+            'profile_photo_path' => null,
+            'current_team_id' => null
+        ]);
+    }
+}
