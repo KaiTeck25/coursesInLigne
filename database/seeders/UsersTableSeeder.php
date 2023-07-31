79ebedcd052d8f5e26bed76bee50c6fa9b073eec
Sofien <sofienbouafia@gmail.com>
2023-07-31 14:55:33 +0100

    login22

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -18,9 +18,22 @@ class UsersTableSeeder extends Seeder
     {
         User::create([
             'name' => 'coder',
+            'status' => '0',
             'email' => 'coder@gmail.com',
             'email_verified_at' => now(),
             'password' => Hash::make(12345678), // password
+            'two_factor_secret' => null,
+            'two_factor_recovery_codes' => null,
+            'remember_token' => Str::random(10),
+            'profile_photo_path' => null,
+            'current_team_id' => null
+        ]);
+        User::create([
+            'name' => 'user',
+            'email' => 'user@tdo.com',
+            'password' => Hash::make('12345678'),
+            'email_verified_at' => time(),
+            'status' => '1',
             'two_factor_secret' => null,
             'two_factor_recovery_codes' => null,
             'remember_token' => Str::random(10),
